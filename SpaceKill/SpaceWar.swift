5da8dfd0fe2f2e1d998394e17e1310181bbc2221
eleves <eleves@marcospaulo.local>
2017-12-04 15:44:20 -0500

    working

---
+++ SpaceKill/SpaceWar.swift
@@ -0,0 +1,33 @@
+//
+//  SpaceWar.swift
+//  SpaceKill
+//
+//  Created by eleves on 17-12-04.
+//  Copyright © 2017 marcos. All rights reserved.
+//
+
+import UIKit
+
+class SpaceWar: UIViewController
+{
+	//------------- Outlets -------------
+	@IBOutlet weak var normandy: UISlider!
+	@IBOutlet weak var view_mothership: UIView!
+	@IBOutlet weak var img_mothership: UIImageView!
+	@IBOutlet weak var view_laquais1: UIView!
+	@IBOutlet weak var img_laquais1: UIImageView!
+	
+	
+	//-----------------------------------
+    override func viewDidLoad()
+	{
+        super.viewDidLoad()
+
+		
+    }
+
+
+
+	
+
+}
