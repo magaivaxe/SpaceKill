26f63ef93cd9955b39fea46f519fe67d3f161ce7
sire_marcos <sire_marcos@marcoss-imac.local>
2017-12-07 02:49:12 -0500

    working

--- SpaceKill/SpaceWar.swift
+++ SpaceKill/SpaceWar.swift
@@ -22,18 +22,19 @@ class SpaceWar: UIViewController
 	@IBOutlet weak var img_normandy: UIImageView!
 	
 	@IBOutlet weak var slider_normandy: UISlider!
-	@IBOutlet weak var button_startGame: UIButton!
+	@IBOutlet weak var button_shot: UIButton!
+	
 	//-----------------------------------
 	//------------ Variables ------------
 	var shotX, shotY: Float!
-	var arrayBullets: [UIView]!
+	var arrayBullets = [UIView]()
 	
 	var animationTimer: Timer!		/* Variable of time animation */
 	var distance = 0				/* Incremental distance to animate */
-	var maxDistanceByScreen: Int!	/* Max distance to animate by screen */
+	var maxDistance: Int!			/* Max distance to animate by screen */
 	var nBullets: Int!				/* Bullets number to shot, the game can change the value */
-	var mode_animationY: CGFloat!	/* Distance on pixels to animate */
-	//var shotAnimating =
+	var animationY: CGFloat!		/* Distance on pixels to animate */
+	var shotSpeed: Double!			/* To apply to time interval animation */
 	//-----------------------------------
 	//============================ The loader =============================
     override func viewDidLoad()
@@ -45,61 +46,12 @@ class SpaceWar: UIViewController
 		//-----
     }
 	//=====================================================================
-	//========================== Game Fonctions ===========================
-	func shot(_ arrayShots: [UIView])
-	{	/* Infinit shots */
-		let i = 0; while i < 1
-		{
-			placeBulletsForShot(arrayShots)
-			
-			for shots in arrayShots
-			{
-				/* UIview adds on view */
-				self.view.addSubview(shots)
-				
-				animateShot(arrayBullets, 0.001, maxDistanceByScreen, 1)
-			}
-		}
-	}
-	//----------- Place bullets for shot ---------
-	func placeBulletsForShot(_ arrayBullets: [UIView])
-	{
-		for bullet in arrayBullets
-		{
-			bullet.center.x = CGFloat(shotX - 2.5)			/* To spacehipCenter: less half viewShot width */
-			bullet.center.y = CGFloat(shotY - 45)			/* less 45 pixels de la normandy */
-		}
-	}
-	//--------------------------------------------
-	func animateShot(_ bulletsToShot: [UIView],
-					 _ timeInterval: Double,
-					 _ maxDistance: Int,
-					 _ animationY: CGFloat)			/* Distace for shot animation */
-	{
-		/* Animation timer execution */
-		animationTimer = Timer.scheduledTimer(timeInterval: timeInterval,
-											  target: self,
-											  selector: #selector(animation),
-											  userInfo: (bulletsToShot, maxDistance, animationY),
-											  repeats: true)
-	}
 	
-	@objc func animation(bulletsToShot: [UIView], maxDistance: Int, animationY: CGFloat)
-	{	/* distance incremental */
-		distance += 1
-		/* Stop the animation */
-		if distance >= maxDistance { animationTimer.invalidate(); animationTimer = nil }
-		
-		/* Animation */
-		for i in 0..<bulletsToShot.count { bulletsToShot[i].center.y += animationY }
-	}
-	
-	//=====================================================================
 	//======================== Loading Fonctions ==========================
 	//------------- Shots creations -------------
 	func spaceshipBulletsCreation(_ nBullets: Int)
 	{
-		for _ in 0...nBullets
+		for _ in 1...nBullets
 		{
 			let bullet = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 10))
 			
@@ -112,7 +64,8 @@ class SpaceWar: UIViewController
 	{
 		//to test
 		nBullets = 1
-		mode_animationY = 1
+		animationY = 1
+		shotSpeed = 0.001
 	}
 	//-------------------------------------------
 	//---------- Slider Configuration ----------- 	SET HERE THE MAX DISTANCE TO ANIMATION
@@ -130,7 +83,7 @@ class SpaceWar: UIViewController
 		shotY = Float(view.frame.height * 0.9017)				/* Initial shot Y value */
 		//------------------
 		//-- Animations config -
-		maxDistanceByScreen = Int(view.frame.height - view.frame.height * 0.0983)
+		maxDistance = Int(view.frame.height - view.frame.height * 0.0983)
 		//----------------------
 		/* Actualization of max and min slider values by mobiles screen sizes */
 		if view.frame.width <= 414				/* All iPhones*/
@@ -163,14 +116,13 @@ class SpaceWar: UIViewController
 	//=========================== Game Actions ============================
 	
 	//------------ Normandy's shifting ----------
-	@IBAction func start_game(_ sender: UIButton)
+	@IBAction func shot_button(_ sender: UIButton)
 	{
-		shot(arrayBullets)
-		
-		
-		
-		//viewBullet.frame.intersection(<#T##r2: CGRect##CGRect#>) conditions to destroid
+		shot()
 	}
+	
+		//viewBullet.frame.intersection(<#T##r2: CGRect##CGRect#>) conditions to destroid
+	
 	//-------------------------------------------
 	//------------ Normandy's shifting ----------
 	@IBAction func shifting_normandy(_ sender: UISlider)
@@ -180,6 +132,57 @@ class SpaceWar: UIViewController
 	}
 	//-------------------------------------------
 	//=====================================================================
+	//========================== Game Fonctions ===========================
+	func shot()
+	{	/* Infinit shots */
+		//		let i = 0; while i < 1
+		//		{
+		placeBulletsForShot(arrayBullets)
+		
+		for shot in arrayBullets
+		{
+			/* UIview adds on view */
+			self.view.addSubview(shot)
+			
+			animateShot(shot)
+		}
+		//		}
+	}
+	//----------- Place bullets for shot ---------
+	func placeBulletsForShot(_ arrayBullets: [UIView])
+	{
+		for bullet in arrayBullets
+		{
+			bullet.center.x = CGFloat(shotX - 2.5)			/* To spacehipCenter: less half viewShot width */
+			bullet.center.y = CGFloat(shotY - 45)			/* less 45 pixels de la normandy */
+		}
+	}
+	//--------------------------------------------
+	func animateShot(_ shot: UIView)
+	{
+		//- Inicial distace for shot animation
+		distance = 0
+		//- Animation timer execution -
+		animationTimer = Timer.scheduledTimer(timeInterval: shotSpeed,
+											  target: self,
+											  selector: #selector(animation),
+											  userInfo: shot,
+											  repeats: true)
+	}
+	
+	@objc func animation(_ aShot: UIView)
+	{	/* distance incremental */
+		distance += 1
+		/* Stop the animation */
+		if distance >= maxDistance { animationTimer.invalidate(); animationTimer = nil }
+		
+		/* Animation */
+		aShot.center.y += animationY
+		//for i in 0..<arrayBullets.count { arrayBullets[i].center.y += animationY }
+	}
+	
+	//=====================================================================
+	
 }
 
 
