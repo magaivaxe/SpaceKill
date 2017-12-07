25f9011d4414f763bc4a461ab715e339921ded52
eleves <eleves@marcospaulo.local>
2017-12-07 17:08:24 -0500

    working

--- SpaceKill/Create.swift
+++ SpaceKill/Create.swift
@@ -11,19 +11,59 @@ import UIKit
 
 class Create
 {
-	func createLackeys(numberOfLackeys nl: Int, screenWidthSize sws: CGFloat, screemHeightSize shs: CGFloat) -> [UIView]
+	func createLackeys(numberOfLackeys nl: Int,
+					   lackeysByLine lb: Int,
+					   screenWidthSize sws: CGFloat,
+					   screemHeightSize shs: CGFloat) -> [UIView]
 	{
-		for _ in 1...nl
+		var arrayToReturn = [UIView]()
+		var aUIView: UIView!
+		var aUIImgView: UIImageView!
+		
+		for i in 1...nl
 		{
-			let lackeyUIView = UIView(frame: .init(x: <#T##CGFloat#>,
-												   y: <#T##CGFloat#>,
-												   width: sws * 0.065,		//0.065 = 50/768 width ipad 9.7"
-												   height: sws * 0.065))
+			//-- Line 1 -- 0.065 = 50/768 width ipad 9.7"
+			if i <= lb
+			{
+				aUIView = UIView(frame: .init(x: 0, y: 0,
+											  width: sws * 0.0651, height: sws * 0.0651))
 
-			let lackeyUIImageView = UIImageView(frame: .init(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>))
-
-
+				aUIImgView = UIImageView(frame: .init(x: 0, y: 0,
+													  width: sws * 0.065, height: sws * 0.065))
+			}
+			//-- Line 2 --
+			if (i > lb && i <= lb * 2)
+			{
+				aUIView = UIView(frame: .init(x: 0, y: 0,
+											  width: sws * 0.0651, height: sws * 0.0651))
+				
+				aUIImgView = UIImageView(frame: .init(x: 0, y: 0,
+													  width: sws * 0.0651, height: sws * 0.0651))
+			}
+			//-- Line 3 --
+			if (i > lb * 2 && i <= lb * 3)
+			{
+				aUIView = UIView(frame: .init(x: 0, y: 0,
+											  width: sws * 0.0651, height: sws * 0.0651))
+				
+				aUIImgView = UIImageView(frame: .init(x: 0, y: 0,
+													  width: sws * 0.0651, height: sws * 0.0651))
+			}
+			//-- Line 4 --
+			if (i > lb * 3 && i <= nl)
+			{
+				aUIView = UIView(frame: .init(x: 0, y: 0,
+											  width: sws * 0.0651, height: sws * 0.0651))
+				
+				aUIImgView = UIImageView(frame: .init(x: 0, y: 0,
+													  width: sws * 0.0651, height: sws * 0.0651))
+			}
+			aUIView.addSubview(aUIImgView)
+			
+			arrayToReturn.append(aUIView)
+			
 		}
+		return arrayToReturn
 	}
 }
 
