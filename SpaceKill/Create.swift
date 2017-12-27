e70572f0a3bcf23843f8a40dd7f0e2bf8e1ef659
sire_marcos <sire_marcos@marcoss-imac.local>
2017-12-27 15:33:16 -0500

    working

--- SpaceKill/Create.swift
+++ SpaceKill/Create.swift
@@ -6,35 +6,76 @@
 //  Copyright © 2017 marcos. All rights reserved.
 //
 
+//------- Libraries -------
 import Foundation
 import UIKit
+//-------------------------
 
+/*********************************************************************************************************
+*																										 *
+*		CREATE CLASS - It create the lackeys and add the UIImageView to Lackeys UIViews					 *
+*																										 *
+*		sws = Screen width size																			 *
+*		shs = Screen heigth size																		 *
+*		dvx = Distance between views on x																 *
+*		dvy = Distance between views on y																 *
+**********************************************************************************************************/
 class Create
 {
-	var arrayToReturn = [AnyObject]()
+	//------- Variables -------
+	var arrayUIView = [UIView]()
 	var aUIView: UIView!
-	var aUIImgView: UIImageView!
-	
-	
-	func createArrayOfLackeys(numberOfLackeys nl: Int,
-							  lackeysByLine lb: Int,
-							  screenWidthSize sws: CGFloat,
-							  screemHeightSize shs: CGFloat,
-							  distanceViewsX dvx: CGFloat,
-							  distanceViewsY dvy: CGFloat) -> [UIView]
+	var mv: UIView!
+	var sws: CGFloat!
+	var shs: CGFloat!
+	var dvx: CGFloat!
+	var dvy: CGFloat!
+	var ipx: CGFloat!
+	var ipy: CGFloat!
+	//------- Constants -------
+	let nl: Int!
+	let lb: Int!
+	//------- Objects --------
+	let inside = Inside()
+	//-------------------------
+	/* The init perform the viewDidLoad's role and
+	it do the constants and variables importations from MainViewController */
+	init(mainView mv: UIView,
+		 numberOfLackeys nl: Int,
+		 lackeysByLine lb: Int,
+		 initialPositionX ipx: CGFloat,
+		 initialPositionY ipy: CGFloat)
 	{
-		let constIncrementationX: CGFloat = dvx / sws
-		let constIncrementationY: CGFloat = dvy / shs
+		//-- Vars to import
+		self.mv = mv					/* Self do the class assignments variables for the imported variables */
+		self.nl = nl
+		self.lb = lb
+		self.ipx = ipx
+		self.ipy = ipy
+		//-- Vars to load --
+		sws = mv.frame.width
+		shs = mv.frame.height
+		dvx = CGFloat(Int(sws)/(lb + 1))
+		dvy = dvx
+	}
+	
+	//================================= FUNCTIONS =================================
+	func createArrayOfLackeys() -> [UIView]
+	{
+		let constIncrementationX: CGFloat = dvx
+		let constIncrementationY: CGFloat = dvy
 		var incX: CGFloat = 0
 		var incY: CGFloat = 0
+		let imageView = inside.createArrayOfImgViewsLackeys(screenWidthSize: sws,
+															screemHeightSize: shs)
 		
 		for i in 1...nl
 		{
 			//-- Line 1 -- 0.0651 = 50/768 width ipad 9.7" // 0.141927 = 109/768 // 0.152344 = 156/1024
 			if i <= lb
 			{
-				aUIView = UIView(frame: .init(x: sws * (109/768 + incX),
-											  y: shs * (156/1024 + incY),
+				aUIView = UIView(frame: .init(x: ipx + incX,		/* UIView creation */
+											  y: ipy + incY,
 											  width: sws * 50/768,
 											  height: shs * 50/1024))
 			}
@@ -43,8 +84,8 @@ class Create
 			{
 				if i == (lb + 1) { incX = 0 } // Reset incX on line 2
 				
-				aUIView = UIView(frame: .init(x: sws * (109/768 + incX),
-											  y: shs * (156/1024 + incY),
+				aUIView = UIView(frame: .init(x: ipx + incX,
+											  y: ipy + incY,
 											  width: sws * 50/768,
 											  height: shs * 50/1024))
 			}
@@ -53,8 +94,8 @@ class Create
 			{
 				if i == (2 * lb + 1) { incX = 0 } // Reset incX on line 3
 				
-				aUIView = UIView(frame: .init(x: sws * (109/768 + incX),
-											  y: shs * (156/1024 + incY),
+				aUIView = UIView(frame: .init(x: ipx + incX,
+											  y: ipy + incY,
 											  width: sws * 50/768,
 											  height: shs * 50/1024))
 			}
@@ -63,39 +104,45 @@ class Create
 			{
 				if i == (3 * lb + 1) { incX = 0 } // Reset incX on line 3
 				
-				aUIView = UIView(frame: .init(x: sws * (109/768 + incX),
-											  y: shs * (156/1024 + incY),
+				aUIView = UIView(frame: .init(x: ipx + incX,
+											  y: ipy + incY,
 											  width: sws * 50/768,
 											  height: shs * 50/1024))
 			}
+			aUIView.addSubview(imageView)							/* UIImgView addiction */
 			
-			arrayToReturn.append(aUIView)
+			arrayUIView.append(aUIView)
 			//-- X incrementations
 			incX = incX + constIncrementationX
 			//-- Y incrementations conditions
 			if (i == lb || i == 2 * lb || i == 3 * lb)
 			{ incY = incY + constIncrementationY }
 		}
-		return arrayToReturn as! [UIView]
+		return arrayUIView
 	}
-	
-	func createArrayOfImgViewsLackeys(numberOfImgLackeys nl: Int,
-									  screenWidthSize sws: CGFloat,
-									  screemHeightSize shs: CGFloat) -> [UIImageView]
+	//=============================================================================
+}
+/*********************************************************************************************************
+*																										 *
+*												INTERNAL CLASS											 *
+*																										 *
+**********************************************************************************************************/
+class Inside
+{
+
+	func createArrayOfImgViewsLackeys(screenWidthSize sws: CGFloat,
+									  screemHeightSize shs: CGFloat) -> UIImageView
 	{
-		for _ in 1...nl
-		{
-			aUIImgView = UIImageView(frame: .init(x: 0,
+		let aUIImgView = UIImageView(frame: .init(x: 0,
 												  y: 0,
 												  width: sws * 50/768,
 												  height: shs * 50/1024))
 			
-			aUIImgView.image = UIImage(named: "lackey.png")
-			arrayToReturn.append(aUIImgView)
-			
-		}
-		return arrayToReturn as! [UIImageView]
+		aUIImgView.image = UIImage(named: "lackey.png")
+		
+		return aUIImgView
 	}
+	
 }
 
 
